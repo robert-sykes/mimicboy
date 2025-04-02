15096dd305e2c5bb34af6d12f19ee5cbbccee9fe
robert-sykes <me@robsykes.dev>
2025-04-02 21:46:45 +0000

    Deploying to gh-pages from @ robert-sykes/mimicboy@df3ed2a20b9b98336006f0c3edccdd3ae362179f 🚀

--- docs/search/all_6.js
+++ docs/search/all_6.js
@@ -1,7 +1,21 @@
 var searchData=
 [
-  ['get_0',['get',['../da/d78/classMimicBoy_1_1Register16.html#ad874b5de8a7791db8a98ea1d880c89d5',1,'MimicBoy::Register16::get()'],['../d3/d98/classMimicBoy_1_1Register8.html#a45257248be4e651cee24138d1bb70a06',1,'MimicBoy::Register8::get()']]],
-  ['getflag_1',['getFlag',['../da/d2b/classMimicBoy_1_1CPU.html#a36b1c8a8b95e70c7a8bc70d0ab3a4452',1,'MimicBoy::CPU']]],
-  ['gethigh_2',['getHigh',['../da/d78/classMimicBoy_1_1Register16.html#aee7e01d4acb7945ffb56fe4089f89443',1,'MimicBoy::Register16']]],
-  ['getlow_3',['getLow',['../da/d78/classMimicBoy_1_1Register16.html#abe7c69496a35acd8ed2b93b850bf87a6',1,'MimicBoy::Register16']]]
+  ['get_0',['get',['../d3/d98/classMimicBoy_1_1Register8.html#a45257248be4e651cee24138d1bb70a06',1,'MimicBoy::Register8::get()'],['../da/d78/classMimicBoy_1_1Register16.html#ad874b5de8a7791db8a98ea1d880c89d5',1,'MimicBoy::Register16::get()']]],
+  ['geta_1',['getA',['../da/d2b/classMimicBoy_1_1CPU.html#a45967035edddd9b6086698e66ee00323',1,'MimicBoy::CPU']]],
+  ['getaf_2',['getAF',['../da/d2b/classMimicBoy_1_1CPU.html#a1e11e3820a8e95d67ca4cf8bbef8e514',1,'MimicBoy::CPU']]],
+  ['getb_3',['getB',['../da/d2b/classMimicBoy_1_1CPU.html#ac05c12559c122c1c12515065575cc80b',1,'MimicBoy::CPU']]],
+  ['getbc_4',['getBC',['../da/d2b/classMimicBoy_1_1CPU.html#a2a6570ca5f4d744c4a758d5b31cf3a0b',1,'MimicBoy::CPU']]],
+  ['getc_5',['getC',['../da/d2b/classMimicBoy_1_1CPU.html#ae4465ded618bff7b1ae8c1d00f5b148c',1,'MimicBoy::CPU']]],
+  ['getd_6',['getD',['../da/d2b/classMimicBoy_1_1CPU.html#aee2e9b5f3138900aaa75a164e144aafa',1,'MimicBoy::CPU']]],
+  ['getde_7',['getDE',['../da/d2b/classMimicBoy_1_1CPU.html#a3ecacfbd8388f9e8a6cce625dab3d6a3',1,'MimicBoy::CPU']]],
+  ['gete_8',['getE',['../da/d2b/classMimicBoy_1_1CPU.html#af399ca48961adfc6c94f3dab6fea5d3a',1,'MimicBoy::CPU']]],
+  ['getf_9',['getF',['../da/d2b/classMimicBoy_1_1CPU.html#ad2fabc5938a31917452d0c9b8510e138',1,'MimicBoy::CPU']]],
+  ['getflag_10',['getFlag',['../da/d2b/classMimicBoy_1_1CPU.html#a36b1c8a8b95e70c7a8bc70d0ab3a4452',1,'MimicBoy::CPU']]],
+  ['geth_11',['getH',['../da/d2b/classMimicBoy_1_1CPU.html#a76f71a94d020cf0ff6d7d35831868e5d',1,'MimicBoy::CPU']]],
+  ['gethigh_12',['getHigh',['../da/d78/classMimicBoy_1_1Register16.html#aee7e01d4acb7945ffb56fe4089f89443',1,'MimicBoy::Register16']]],
+  ['gethl_13',['getHL',['../da/d2b/classMimicBoy_1_1CPU.html#a31118b5839558c9b33debd42079dc61d',1,'MimicBoy::CPU']]],
+  ['getl_14',['getL',['../da/d2b/classMimicBoy_1_1CPU.html#a6d8721b4af70d764c0a98697051d9bf4',1,'MimicBoy::CPU']]],
+  ['getlow_15',['getLow',['../da/d78/classMimicBoy_1_1Register16.html#abe7c69496a35acd8ed2b93b850bf87a6',1,'MimicBoy::Register16']]],
+  ['getpc_16',['getPC',['../da/d2b/classMimicBoy_1_1CPU.html#a81a7618a12b015068ed57dfaf4c6a03c',1,'MimicBoy::CPU']]],
+  ['getsp_17',['getSP',['../da/d2b/classMimicBoy_1_1CPU.html#a4dd2fd410f6c492486e85e4fc8223152',1,'MimicBoy::CPU']]]
 ];
