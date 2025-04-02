c61d35fcc1a79f1b4c1be23b0455f8c2b6b35975
Rob Sykes <me@robsykes.dev>
2025-04-02 23:39:18 +0100

    🔥 Remove MemoryTest from unit tests
    MemoryTest has been deleted and its references were removed from the CMake configuration and CTest registration. This cleanup ensures unused tests are no longer included in the codebase.
    
    Signed-off-by: Rob Sykes <me@robsykes.dev>

--- tests/unit/MemoryTest.cc
+++ tests/unit/MemoryTest.cc
@@ -1,9 +1,3 @@
 #include <gtest/gtest.h>
 #include "src/Memory.h"
 
-TEST(MemoryTest, ReadWrite) {
-    MimicBoy::Memory memory;
-
-    memory.write(0x1234, 0xAB);
-    EXPECT_EQ(memory.read(0x1234), 0xAB);
-}
